2f1c099115747d0c09de76dcebe0197c534ab648
Baruch Baksht <51882386+bakshtb@users.noreply.github.com>
2021-11-10 22:36:40 +0200

    debugging

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import collections
 import datetime
 
-from gensim.similarities.docsim import query_shard
-
 import util
-import math
 from tabulate import tabulate
 import os
 from collections import Counter
@@ -12,7 +9,7 @@ import numpy as np
 from scipy import spatial
 from tqdm import tqdm
 import textwrap
-import pandas as pd
+import pickle
 
 
 def make_dictionary(file):
@@ -54,32 +51,42 @@ def calculate_TF_IDF(wordset, tf_diz, idf_diz):
 
 
 def main():
-    query_str = "ליאונל מסי"
+    query_str = "נשיא טראמפ ביידן ארצות הברית"
     docs_files = ['data\\' + file for file in os.listdir('data') if file.endswith(".txt")]
     #query_files = ['query\\' + file for file in os.listdir('query') if file.endswith(".txt")]
-    all_files = docs_files[1:1000]
+    all_files = docs_files[1:10000]
 
     files_words = {}
-    for file in tqdm(all_files):
-        files_words[file] = make_dictionary(file)
-    files_words["Query"] = query_str.split()
-
     all_words = query_str.split()
     for file in tqdm(all_files):
+        files_words[file] = make_dictionary(file)
         all_words += make_dictionary(file)
+    files_words["Query"] = query_str.split()
     all_words = Counter(all_words).most_common()
+
+    with open('1.pkl', 'wb') as f:
+        pickle.dump(query_str, f)
+        pickle.dump(all_files, f)
+        pickle.dump(files_words, f)
+        pickle.dump(all_words, f)
 
     all_words_temp = []
     for word in tqdm(all_words):
-        if word[1] > 5 and len(word[0]) > 1 and word[0] not in util.get_stop_words():
+        if word[1] > 111 and len(word[0]) > 1 and word[0] not in util.get_stop_words():
             all_words_temp += [word[0]]
     all_words = all_words_temp
     #print(all_words)
 
-    avdl = np.average([len(bow) for bow in list(files_words.values())])
+    with open('2.pkl', 'wb') as f:
+        pickle.dump(all_words_temp, f)
 
+    avdl = np.average([len(bow) for bow in list(files_words.values())])
     IDF = calculate_IDF(all_words, list(files_words.values()))
     #print(IDF)
+
+    with open('3.pkl', 'wb') as f:
+        pickle.dump(avdl, f)
+        pickle.dump(IDF, f)
 
     cosine_distances = []
     euclidean_distances = []
@@ -90,11 +97,14 @@ def main():
             cosine_distances.append([file, spatial.distance.cosine(query_TF_IDF, doc_TF_IDF)])
             euclidean_distances.append([file, spatial.distance.euclidean(query_TF_IDF, doc_TF_IDF)])
 
-
     cosine_distances = sorted(cosine_distances, key=lambda x: x[1])
     cosine_distances = [it for it in cosine_distances if it[1] != 0]
     euclidean_distances = sorted(euclidean_distances, key=lambda x: x[1])
     euclidean_distances = [it for it in euclidean_distances if it[1] != 0]
+
+    with open('4.pkl', 'wb') as f:
+        pickle.dump(cosine_distances, f)
+        pickle.dump(euclidean_distances, f)
 
     print(tabulate(cosine_distances[:10], headers=['file', 'cosine distances'], tablefmt='fancy_grid'))
     print(tabulate(euclidean_distances[:10], headers=['file', 'euclidean distances'], tablefmt='fancy_grid'))
@@ -114,3 +124,10 @@ if __name__ == '__main__':
     main()
     toc = datetime.datetime.now()
     print('\n' + str((toc - tic)))
+
+
+def process_TFIDF(file, ):
+    if file != "Query":
+        doc_TF_IDF = calculate_TF_IDF(all_words, calculateTF(all_words, files_words[file], avdl), IDF)
+        cosine_distances.append([file, spatial.distance.cosine(query_TF_IDF, doc_TF_IDF)])
+        euclidean_distances.append([file, spatial.distance.euclidean(query_TF_IDF, doc_TF_IDF)])
